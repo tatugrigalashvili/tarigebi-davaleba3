12d0cde38d99e6925db9132e630702a71e97f0a8
tatugrigalashvili <146704405+tatugrigalashvili@users.noreply.github.com>
2024-06-19 16:04:42 +0400

    Add files via upload

---
+++ davaleba.js
@@ -0,0 +1,20 @@
+const now = new Date();
+
+const weli = now.getFullYear();
+console.log(weli);
+
+
+const tve = now.getMonth() + 1;
+console.log(tve);
+
+
+const wuti = now.getMinutes();
+console.log(wuti);
+
+
+const wami = now.getSeconds();
+console.log(wami);
+
+
+const kvira =now.getDay();
+console.log(kvira);
